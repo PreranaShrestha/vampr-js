0db5d53526d047547d5de6fd4ebf9c514f0e7a36
PreranaShrestha <prerana.sh@gmail.com>
2018-02-16 05:02:06 +0000

    add totalDescendents

--- vampire.js
+++ vampire.js
@@ -10,21 +10,87 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let vampire = this;
+    while(vampire.creator) {
+      numberOfVampires++;
+      vampire = vampire.creator;
+    }
+    return(numberOfVampires);
+  }
 
+  vampireWithName(name) {
+    var vampireWithGivenName =[];
+    if (this.name === name) {
+      vampireWithGivenName.push(this);
+    }
+      for (const offsp of this.offspring) {
+        const offspName = offsp.vampireWithName(name);
+        vampireWithGivenName = vampireWithGivenName.concat(offspName);
+    }
+      return vampireWithGivenName;
+  }
+
+
+  get allMillennialVampires() {
+    var millennnialVampires = [];
+    if(this.yearConverted > 1980) {
+      millennnialVampires.push(this);
+    }
+      for(const offsp of this.offspring){
+        const millennialVamp = offsp.allMillennialVampires;
+        millennnialVampires = millennnialVampires.concat(millennialVamp);
+
+      }
+    return millennnialVampires;
+
+  }
+
+
+
+  get totalDescendents() {
+    var totalDes = [];
+    totalDes.push(this);
+      for(const offsp of this.offspring){
+        const descendent = offsp.totalDescendents;
+        totalDes= totalDes.concat(descendent);
+
+      }
+    return totalDes.length;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let numberOfVampires1 = 0;
+    let vampire1 = this;
+    while(vampire1.creator) {
+      numberOfVampires1++;
+      vampire1 = vampire1.creator;
+    }
+    let numberOfVampires2 = 0;
+    let vampire2 = vampire;
+    while(vampire2.creator) {
+      numberOfVampires2++;
+      vampire2 = vampire2.creator;
+    }
+    if(numberOfVampires1 < numberOfVampires2){
+      return true;
+    } else {
+      return false;
+    }
 
   }
 
@@ -36,9 +102,52 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+  //   let numberOfVampires1 = 0;
+  //   let vampire1 = this;
+  //   while(vampire1.creator) {
+  //     numberOfVampires1++;
+  //     vampire1 = vampire1.creator;
+  //   }
+  //   let numberOfVampires2 = 0;
+  //   let vampire2 = vampire;
+  //   while(vampire2.creator) {
+  //     numberOfVampires2++;
+  //     vampire2 = vampire2.creator;
+  //   }
+  //   if(numberOfVampires1 < numberOfVampires2){
+  //     return vampire1;
+  //   } else {
+  //     return vampire2;
+  //   }
+
+  // }
+
+
+
+
 
   }
 }
+
+const original = new Vampire("Original", "1980");
+const bart = new Vampire("Bart", "1981");
+const ansel = new Vampire("Ansel", "1982");
+original.addOffspring(bart);
+original.addOffspring(ansel);
+
+
+const elgort = new Vampire("Elgort", "1984");
+const sarah = new Vampire("Sarah", "1985");
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+
+const andrew = new Vampire("Andrew", "1990");
+elgort.addOffspring(andrew);
+
+
+console.log(original.vampireWithName("Andrew"));
+// console.log(ansel.numberOfOffspring);
+
 
 module.exports = Vampire;
 
